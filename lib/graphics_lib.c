89e11c7c52162bc17fdf0a46fc748b31456a44a7
zycczy <421579739@qq.com>
2016-10-27 17:52:40 +0800

    add separate maritx

--- lib/graphics_lib.c
+++ lib/graphics_lib.c
@@ -52,8 +52,58 @@ HBMP_i_t* bmp_parser(char *scr_file, char *dst_file)
 	fclose(file);
 	return hbmp_buf;
 }
+typedef struct _MARITX_HBMP
+{
+	HBMP_i_t* hbmp;
+	uint32_t  height_coordinate;
+	uint32_t  width_coordinate;
+}MARITX_HBMP;
 
 
+uint32_t separate_maritx(HBMP_i_t* hbmp, HBMP_i_t **dst)
+{
+	uint32_t width_copies = hbmp->width / dst[0]->width;
+	uint32_t height_copies = hbmp->height / dst[0]->height;
+	uint32_t copies_count = width_copies * height_copies;
+	MARITX_HBMP* maritx;
+	uint32_t i,j,t;
+	if(hbmp->height%dst[0]->height != 0 && hbmp->width%dst[0]->width != 0){
+		__wrn("cannot separate the picture average!\n");
+		return EPDK_FAIL;
+	}
+	//specify the coordinate
+	t = 0;
+	maritx = (MARITX_HBMP *)malloc(sizeof(MARITX_HBMP)*copies_count);
+	while(t<copies_count){
+		for(i=0;i<height_copies;i++){
+			for(j=0;j<width_copies;j++){
+				maritx[copies_count].hbmp = dst[copies_count];
+				maritx[copies_count].height_coordinate = i;
+				maritx[copies_count].width_coordinate = j;
+				
+				t++;
+			}
+		}
+	}
+	
+	for(t=0;t<copies_count;t++){
+		for(i=0;i<maritx[copies_count].hbmp->height;i++){
+			memcpy(maritx[copies_count].hbmp->rgb_buffer+(maritx[copies_count].hbmp->width*i),\
+				   hbmp->rgb_buffer+(maritx[copies_count].height_coordinate*t)+maritx[copies_count].width_coordinate*maritx[copies_count].hbmp->width,\
+				   maritx[copies_count].hbmp->width);
+		}
+	}
+	#if 0
+	for(t=0;t<copies_count;t++){
+		for(i=0;i<dst->height;i++){
+			for(j=0;j<width_copies;j++){
+				memcpy(((*(dst+t))+(dst->width*i)), hbmp->rgb_buffer+)
+			}
+		}
+	}
+	#endif
+	return EPDK_OK;
+}
 
 
 
